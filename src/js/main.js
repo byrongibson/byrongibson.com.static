ce15267b956b9267ccaeb147739a755e2be76b4b
Byron Gibson <fbg111@gmail.com>
2013-10-18 20:00:50 -0700

    update: removed .min from require.js imports, all js now minified by default in prod

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,12 @@
 // async load libs
 // http://www.requirejs.org/docs/start.html
 require([
-    "lib/bootstrap.min", 
-    "lib/jquery.min", 
-    "lib/react.min", 
-    "lib/JSXTransformer.min", 
-    "lib/d3.min", 
-    "plugins.min"
+    "lib/jquery", 
+    "lib/bootstrap", 
+    "lib/react", 
+    "lib/JSXTransformer", 
+    "lib/d3", 
+    "plugins"
 ], function(util) {
     //This function is called when scripts/helper/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
